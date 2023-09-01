ff65c2635fbcf0bd39075c51bc1e7b300cd8fc46
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-02 01:25:04 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenge 1/script.js
+++ JavaScript Challenge 1/script.js
@@ -1,8 +1,31 @@
 console.log('I am linked now!');
-const myName = "Jana";
-alert(myName);
-let age = 28;
-const lastName = "FD"
-var DOB = "10 July, 1995";
-age = 29;
-console.log(age, lastName, DOB);
+const myName = "Jana"; //Step 2
+alert(myName); //Step 2
+
+let age = 28; //Step 3
+const lastName = "FD" //Step 3
+var DOB = "10 July, 1995"; //Step 3
+age = 29; //Step 3
+console.log(age, lastName, DOB); //Step 3
+
+console.log(typeof("This is a text")); //string
+console.log(typeof(true)); //boolean
+console.log(typeof(false)); //boolean
+console.log(typeof(1215)); //number
+console.log(typeof("999")); //string
+console.log(typeof({})); //object
+console.log(typeof([])); //object
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined
+let var1 = "this is a string"; 
+console.log(`Variable 1: "${var1}" is a ${typeof(var1)}`);
+let var2 = 1294;
+console.log(`Variable 2: ${var2} is a ${typeof(var2)}`);
+let var3 = true;
+console.log(`Variable 3: ${var3} is a ${typeof(var3)}`);
+let var4 = [1,2,3];
+console.log(`variable 4: ${var4} is an ${typeof(var4)}`);
+let var5 = {name: "Jana", age: 24};
+console.log(`Variable 5: ${JSON.stringify(var5)} is an ${typeof(var5)}`);
+let var6 = null;
+console.log(`Variable 6: ${var6} is ${typeof(var6)}`);
