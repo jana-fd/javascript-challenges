753036b91b01ac86d8bc83a3f539be6b956144e1
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-02 01:01:25 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenge 1/script.js
+++ JavaScript Challenge 1/script.js
@@ -1,3 +1,8 @@
 console.log('I am linked now!');
 const myName = "Jana";
 alert(myName);
+let age = 28;
+const lastName = "FD"
+var DOB = "10 July, 1995";
+age = 29;
+console.log(age, lastName, DOB);
