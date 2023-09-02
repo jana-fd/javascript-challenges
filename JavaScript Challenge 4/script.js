fc235c80023a3b905b891f4f8ea394b828fe3008
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-02 22:50:58 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenge 4/script.js
+++ JavaScript Challenge 4/script.js
@@ -1,9 +1,31 @@
+//Step 1
 const fiveNumbers = [-10, 23, 49, 76, -60];
 
 const personalInfo = {
     name: "Jana",
     age: 28,
     height: 170,
-    location: "somewhere",
-    nationality: "some nationality"
+    location: "my location",
+    nationality: "my nationality"
 }
+
+
+//Step 2
+const nextThreeNumbers = [12, 15, 17];
+const allNumbers = [...fiveNumbers, ...nextThreeNumbers];
+console.log(allNumbers);
+
+
+const additionalInfo = {
+    occupation: "programming related",
+    hobby: "learning languages",
+    education: "physics"
+}
+
+const fullInfo = {...personalInfo, ...additionalInfo};
+console.log(fullInfo)
+
+
+
+
+
