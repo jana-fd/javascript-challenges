fe5722a38b062fb46ecbf913c7ade7b3d30ad024
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-02 22:37:02 +0300

    Challenge 4 - Done with step 1

---
+++ JavaScript Challenge 4/script.js
@@ -0,0 +1,9 @@
+const fiveNumbers = [-10, 23, 49, 76, -60];
+
+const personalInfo = {
+    name: "Jana",
+    age: 28,
+    height: 170,
+    location: "somewhere",
+    nationality: "some nationality"
+}
