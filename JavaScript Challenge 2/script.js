51273cd1a711326117bee923c3b8f82863e12e62
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-02 15:23:52 +0300

    Challenge 2 - Done with step 2

--- JavaScript Challenge 2/script.js
+++ JavaScript Challenge 2/script.js
@@ -1,6 +1,6 @@
 //Step 1
-let age = 24;
-if (age >= 20) {
+let myAge = 24;
+if (myAge >= 20) {
     console.log("You can enter this room.");
 }
 else {
@@ -14,3 +14,17 @@ const expression4 = !expression3;
 
 console.log(`5 > 3 is ${expression1}, 10 === '10' is ${expression2}, true && false is ${expression3} and !(true && false) is ${expression4}.`);
 
+
+//Step 2
+const age = 25;
+const hasDriverLicense = true;
+const hasCar = false;
+
+let canDrive = false;
+if (age >= 18 && hasDriverLicense) canDrive = true;
+
+let canRentCar = false;
+if (age >= 22 && hasDriverLicense && !hasCar) canRentCar = true;
+
+let needDriver = false;
+if (age < 18 && !hasDriverLicense && hasCar) needDriver = true;
