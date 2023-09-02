a35e0d68a2be9809c7478bc6d39f448b4ef1a660
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-03 01:59:14 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenge 5.1/script.js
+++ JavaScript Challenge 5.1/script.js
@@ -19,3 +19,34 @@ button.addEventListener('click', () => {
     let randomNumber = Math.floor(Math.random()*1000 + 1);
     button.innerText = `Clicked ${randomNumber}!`
 })
+
+
+//Step 2
+const h1 = document.createElement('h1');
+h1.innerText = "this is an h1 header";
+document.body.appendChild(h1);
+h1.style.border = "10px dotted green";
+document.addEventListener('keydown', (event) => {
+    let letter = prompt('Enter a letter');
+    switch (letter.toUpperCase()) {
+        case 'A':
+            h1.style.backgroundColor = "red";
+            break;
+        case 'S':
+            h1.style.marginLeft = "10px";
+            break;
+        case 'D':
+            const par = document.createElement('p');
+            par.innerText = "Key D was pressed!";
+            document.body.appendChild(par);
+            break;
+        case 'W':
+            h1.style.display = "none";
+            break;
+        case ' ':
+            h1.style.fontSize = "70px";
+        default:
+            alert('Invalid key.');
+
+    }
+})
