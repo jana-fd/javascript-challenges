f72fd1773dbdb3a3bc676007d38a05e647e9bc26
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-02 23:42:15 +0300

    Challenge 4 - Done with step 4

--- JavaScript Challenge 4/script.js
+++ JavaScript Challenge 4/script.js
@@ -6,7 +6,11 @@ const personalInfo = {
     age: 28,
     height: 170,
     location: "Spain",
-    nationality: "Europe"
+    nationality: "Europe",
+    contactInfo: {
+        address: "Main Street",
+        phoneNumber: 1234,
+    }
 }
 
 
@@ -31,9 +35,41 @@ const [firstNum, secondNum, thirdNum] = fiveNumbers;
 console.log(firstNum, secondNum, thirdNum);
 
 const {name, location : loc, nationality} = personalInfo; //Ask about the error, why is it giving an error when I use location alone
-console.log("Name: " + name);
+console.log("Name: " + name); //Ask why name is slashed like this
 console.log("Location: " + loc);
 console.log("Nationality: " + nationality);
 
+
+//Step 4
+
+//Nested Destructuring
+let {contactInfo: { address, phoneNumber }} = personalInfo;
+console.log("Address: " +address);
+console.log("Phone Number: " +phoneNumber);
+
+//Rest with Destructuring
+let [number1, number2, ...restOfNumbers] = fiveNumbers;
+console.log("First number: " +number1);
+console.log("Second number: " +number2);
+console.log("The remaining numbers: " +restOfNumbers);
+
+//Merging and Overwriting with Spread
+const book = {
+    name: "",
+    referenceNumber: "",
+    numberOfPages: "",
+    author: ""
+}
+
+const book1 = {
+    name: "The Lord of the Rings",
+    numberOfPages: 1207,
+    author: "J.R.R. Tolkien"
+}
+
+const generalBook = {...book};
+const firstBook = {...book1};
+
+console.log(Object.assign(generalBook, firstBook));
 
 
