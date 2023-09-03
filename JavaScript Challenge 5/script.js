f66d4de130a21eb4e0f5b862d8f59d4686778546
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-03 18:54:26 +0300

    Challenge 5 - Done with step 2

--- JavaScript Challenge 5/script.js
+++ JavaScript Challenge 5/script.js
@@ -42,3 +42,86 @@ for(const h of headers) {
     h.innerHTML = text.italics(); //Why is it slashed?
 }
 
+
+//Step 2
+
+//Multiple Event Listeners
+const hoverButton = document.createElement('button');
+hoverButton.style.border = " 1px solid black";
+hoverButton.innerHTML = "hover over";
+hoverButton.addEventListener( 'mouseover', () => {
+    hoverButton.style.backgroundColor = "blue";
+})
+hoverButton.addEventListener( 'mouseout', () => {
+    hoverButton.style.backgroundColor = '';
+})
+
+document.body.appendChild(hoverButton);
+
+//Event Delegation
+const buttons = document.getElementById('buttonContainer');
+buttons.addEventListener ('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        event.preventDefault();
+        console.log(event.target.innerHTML);
+    }
+
+})
+
+
+//Form Data Extraction
+const form = document.createElement('form');
+
+//Username label
+const nameLabel = document.createElement('label');
+nameLabel.innerHTML = 'Name: ';
+
+//Username text input
+const nameInput = document.createElement('input');
+nameInput.type = 'text';
+nameInput.name = 'username';
+
+//set Id to username input
+nameInput.setAttribute('id', 'Name');
+form.appendChild(nameLabel);
+form.appendChild(nameInput);
+const break1 = document.createElement('br');
+form.appendChild(break1);
+
+
+//Email label
+const emailLabel = document.createElement('label');
+emailLabel.innerHTML = 'Email: ';
+
+//Email text input
+const email = document.createElement('input');
+email.type = 'text';
+email.name = 'email';
+
+//set Id to email input
+email.setAttribute('id', 'Email');
+form.appendChild(emailLabel);
+form.appendChild(email);
+const break2 = document.createElement('br');
+form.appendChild(break2);
+
+
+//Submit button
+const submitButton = document.createElement('input');
+submitButton.type = 'submit';
+//Value on button
+submitButton.value = 'Submit';
+form.appendChild(submitButton);
+
+document.body.appendChild(form);
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log("Name: " + nameInput.value);
+    console.log("Email: " +email.value);
+});
+
+
+
+
