5fc026cd25e9d1e9287fb958ad8f41d6ab515b75
Jana Fakher Eddine <janafeld.15@gmail.com>
2023-09-03 23:23:55 +0300

    Challenge 6.1 - Done with step 2

--- JavaScript Challenge 6.1/script.js
+++ JavaScript Challenge 6.1/script.js
@@ -1,3 +1,4 @@
+//Step 1
 const delay = (milliseconds) => {
     const promise = new Promise( (resolve, reject) => {
         setTimeout(() => {
@@ -10,4 +11,21 @@ const delay = (milliseconds) => {
 delay(3000)
  .then((result) => {
     console.log(result);
+ })
+
+ //Step 2
+const url = 'https://uselessfacts.jsph.pl/random.json?language=en'
+fetch(url)
+ .then((response) => {
+    if (!response.ok) {
+        throw new Error("Cannot fetch fact");
+    }
+    return response.json();
+ })
+ .then((result) => {
+    const fact = result.text;
+    console.log("Random Fact: ", fact); 
+ })
+ .catch((err) => {
+    console.error(err);
  })
